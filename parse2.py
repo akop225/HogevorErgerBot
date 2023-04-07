9a9a49e3a72e288894577f6b5e56f22bc5b70d5f
Акоп Погосян <jacob@MacBook-Air-Akop.local>
2023-04-07 19:57:56 +0300

    Main last remove_nums

--- parse2.py
+++ parse2.py
@@ -9,7 +9,7 @@ def remove_notes(s):
     i = 0
     while i < len(s):
         el = s[i]
-        #print(el, '---')
+        # print(el, '---')
         if notes in el:
             s.remove(el)
         i += 1
@@ -19,38 +19,15 @@ def remove_notes(s):
 def remove_nums(s):
     i = 0
     while i < len(s):
-        el = s[i].split()
-        k = 0
-        while k < len(el):
-            el2 = el[k]
-            if re.search(r'\d\.', el2) and len(el2.strip()) > 2:
-                el.remove(el2)
-            k += 1
-        #print(' '.join(el), '---')
-        s[i] = ' '.join(el)
-        if re.search(r'\d{2}', s[i]):
-            del s[i]
+        el = s[i]
+        if re.search(r'\d', el) and not (re.search(r'\d\.', el) or re.search(r'\.', el)):
+            s.remove(el)
         i += 1
     return '\n'.join(s).strip()
 
 
-def delete_extra(text):
-    s = text.split()
-    armenian_pattern = r'[(\u0561-\u0587\u0531-\u0556՛՜)]+'
-    for el in s:
-        if re.fullmatch(armenian_pattern, el):
-            pass
-        # if re.fullmatch(r'\d\.', el):
-        #     print(el)
-        #     print(len(el))
-        # print(re.search(r'\(\d+\)', el))
-        # if re.search(r'\(\d+\)', el):
-        #     print(el)
-        #     print(len(el))
 
-
-
-i = '63_20'
+i = 104
 # Define the URL of the website
 url = f'http://www.ergaran.in/2016/01/{i}.html'
 
@@ -59,17 +36,23 @@ response = requests.get(url)
 html_content = response.text
 
 soup = BeautifulSoup(html_content, 'html.parser')
-print(html_content)
+# print(html_content)
 div_tags = soup.find_all('div', {'dir': 'ltr', 'trbidi': 'on'})
+print(div_tags)
 s = str(div_tags[0])
-print(s)
-# print(s.split('<br/>'))
-armenian_pattern = r"[(\d\.\u0561-\u0587\u0531-\u0556՛՜)]+|<br/>"#'<br/>\d+\.*|[(\u0561-\u0587\u0531-\u0556՛՜)]+|<br/>'
+
+armenian_pattern = r"[(\d\.\u0561-\u0587\u0531-\u0556՛՜՜)]+|<br/>"#'<br/>\d+\.*|[(\u0561-\u0587\u0531-\u0556՛՜)]+|<br/>'
 matches = re.findall(armenian_pattern, s)
 print(matches)
 
 text_of_song_not_clear = remove_notes(' '.join(matches).replace('<br/>', '\n').strip().split('\n'))
+print(text_of_song_not_clear)
 text_of_song = remove_nums(text_of_song_not_clear.split('\n'))
-print(text_of_song)
+if text_of_song.find('1.') != -1:
+    k = list(text_of_song)
+    k.insert(text_of_song.find('1.'), '\n')
+    text_of_song = ''.join(k)
+    print('YES')
 with open(f'songs/{i}.txt', 'w') as f:
+    print(text_of_song)
     f.write(text_of_song)
--- parser.py
+++ parser.py
@@ -1,7 +1,15 @@
+import time
 from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 import re
+
+exceptions = {
+    62: '63_20',
+    94: '094',
+    95: '095',
+    96: '096',
+}
 
 
 def remove_notes(s):
@@ -9,6 +17,7 @@ def remove_notes(s):
     i = 0
     while i < len(s):
         el = s[i]
+        # print(el, '---')
         if notes in el:
             s.remove(el)
         i += 1
@@ -18,19 +27,27 @@ def remove_notes(s):
 def remove_nums(s):
     i = 0
     while i < len(s):
-        el = s[i]
-        if re.search(r'\d', el) and not re.search(r'\d\.', el) or re.search(r'\.', el) and not re.search(r'\d\.', el):
-            s.remove(el)
+        el = s[i].split()
+        k = 0
+        while k < len(el):
+            el2 = el[k]
+            if re.search(r'\d\.', el2) and len(el2.strip()) > 2:
+                el.remove(el2)
+            k += 1
+        # print(' '.join(el), '---')
+        s[i] = ' '.join(el)
+        if re.search(r'\d{2}', s[i]):
+            del s[i]
         i += 1
     return '\n'.join(s).strip()
 
 
-for i in range(62, 661):
+for i in range(100, 661):
     # Define the URL of the website
     url = f'http://www.ergaran.in/2016/01/{i}.html'
 
-    if i == 62:
-        url = f'http://www.ergaran.in/2016/01/63_20.html'
+    if i in exceptions:
+        url = f'http://www.ergaran.in/2016/01/{exceptions[i]}.html'
 
     # Send a GET request to the URL and get the HTML content
     response = requests.get(url)
@@ -39,12 +56,19 @@ for i in range(62, 661):
     soup = BeautifulSoup(html_content, 'html.parser')
     # print(html_content)
     div_tags = soup.find_all('div', {'dir': 'ltr', 'trbidi': 'on'})
+    #print(div_tags)
     s = str(div_tags[0])
-
-    armenian_pattern = r'[ (\d.\u0561-\u0587\u0531-\u0556՛՜)]+|<br/>'
+    # print(s)
+    armenian_pattern = r"[(\d\.\u0561-\u0587\u0531-\u0556՛՜)]+|<br/>"
     matches = re.findall(armenian_pattern, s)
+    # print(matches)
 
     text_of_song_not_clear = remove_notes(' '.join(matches).replace('<br/>', '\n').strip().split('\n'))
     text_of_song = remove_nums(text_of_song_not_clear.split('\n'))
     with open(f'songs/{i}.txt', 'w') as f:
         f.write(text_of_song)
+    print('SUCCESSFULLY -', i)
+    print(text_of_song)
+    print('-----' * 10)
+    time.sleep(1)
+
